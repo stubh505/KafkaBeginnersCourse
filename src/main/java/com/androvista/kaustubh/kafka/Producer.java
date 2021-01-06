a32cd315053d34f4f8be7943315f963e2e8cfdcd
stubh505 <kaustubh.sarkar505@gmail.com>
2021-01-06 20:20:49 +0530

    Producer with callback

--- src/main/java/com/androvista/kaustubh/kafka/Producer.java
+++ src/main/java/com/androvista/kaustubh/kafka/Producer.java
@@ -1,13 +1,16 @@
 package com.androvista.kaustubh.kafka;
 
-import org.apache.kafka.clients.producer.KafkaProducer;
-import org.apache.kafka.clients.producer.ProducerConfig;
-import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.clients.producer.*;
 import org.apache.kafka.common.serialization.StringSerializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.Properties;
 
 public class Producer {
+
+    static Logger logger = LoggerFactory.getLogger(Producer.class);
+
     public static void main(String[] args) {
 
         // bootstrap server
@@ -20,13 +23,24 @@ public class Producer {
         properties.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
 
         // create producer
-        KafkaProducer<String, String> producer = new KafkaProducer<String, String>(properties);
+        KafkaProducer<String, String> producer = new KafkaProducer<>(properties);
 
-        // create record
-        ProducerRecord<String, String> record = new ProducerRecord<>("first_topic", "Hello from java side again");
+        for (int i = 190; i<200; i++) {
 
-        // send data
-        producer.send(record);
+            // create record
+            ProducerRecord<String, String> record = new ProducerRecord<>("first_topic", "java side again 0" + i);
+
+            // send data
+            producer.send(record, (recordMetadata, e) -> {
+                if (e == null) {
+                    logger.info("Message received : { topic : " + recordMetadata.topic() + ", " +
+                            "partition : " + recordMetadata.partition() + ", offset : " + recordMetadata.offset() + " }");
+                } else {
+                    logger.error("Error occurred", e);
+                }
+            });
+
+        }
         producer.close();
     }
 }
